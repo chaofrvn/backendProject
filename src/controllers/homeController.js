0982fdd9f9642be6320a35fb5075751b5a9a9b75
Chaofrvn <thai.bh204688@sis.hust.edu.vn>
2022-12-10 20:18:00 +0700

    first commit

---
+++ src/controllers/homeController.js
@@ -0,0 +1,12 @@
+let getHomePage = (req, res) => {
+    return res.render("homepage.ejs");
+}
+let getAboutPage = (req, res) => {
+    return res.render("about.ejs");
+}
+
+module.exports = {
+    getHomePage: getHomePage,
+    getAboutPage: getAboutPage
+    
+}
